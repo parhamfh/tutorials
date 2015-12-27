deddc9868c8f4383b881e5ac88ced0caab595e56
Pärham Fazelzadeh <parhamfh@kth.se>
2015-12-28 00:39:50 +0100

    Add child route for separate child view

--- ember-1/js/routes.js
+++ ember-1/js/routes.js
@@ -1,9 +1,17 @@
 Todos.Router.map(function() {
-  this.resource('todos', { path: "/"});
+  this.resource('todos', { path: "/"}, function(){
+    // add routes:  
+  });
 });
 
 Todos.TodosRoute = Ember.Route.extend({
   model: function(){
     return this.store.find('todoparham');
+  }
+});
+
+Todos.TodosIndexRoute = Ember.Route.extend({
+  model: function() {
+    return this.modelFor('todos');
   }
 });
