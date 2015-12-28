be0dde6b3b250bc74d837a4b9ea152058d3b913b
Pärham Fazelzadeh <parhamfh@kth.se>
2015-12-28 01:14:25 +0100

    Controller action for deleting/counting all compeleted tasks

--- ember-1/js/controllers/todos_controller.js
+++ ember-1/js/controllers/todos_controller.js
@@ -12,6 +12,11 @@ Todos.TodosController = Ember.ArrayController.extend({
       this.set('newTitle', '');
       
       todo.save();
+    },
+    clearCompleted: function() {
+      var completed = this.filterBy('isCompleted', true);
+      completed.invoke('deleteRecord');
+      completed.invoke('save');
     }
   },
 
@@ -22,5 +27,13 @@ Todos.TodosController = Ember.ArrayController.extend({
   inflection: function() {
     var remaining = this.get('remaining');
     return remaining === 1? 'item' : 'items';
-  }.property('remaining')
+  }.property('remaining'),
+
+  hasCompleted: function(){
+    return this.get('completed') > 0;
+  }.property('completed'),
+  
+  completed: function() {
+    return this.filterBy('isCompleted', true).get('length');
+  }.property('@each.isCompleted')
 });
